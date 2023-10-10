01e9e85e115253136e8f3948d5df17ac6ea17c67
Naol Solomon <naolsolomon10@gmail.com>
2023-10-10 03:44:49 +0300

    updated

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for letter in my_string:
-        new_string = my_string.copy()
-        if letter != 'c' or letter != 'C':
-            return new_string
+    copy = [x for x in my_string if x != 'c' and x != 'C']
+    return ("".join(copy))
