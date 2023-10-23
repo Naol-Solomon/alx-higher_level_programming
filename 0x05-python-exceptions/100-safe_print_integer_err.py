f2cea19e26c8335e14180d51bbece18e88afc6b3
Naol Solomon <naolsolomon10@gmail.com>
2023-10-24 02:52:43 +0300

    advanced is done

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as n:
+        print("Exception: {}".format(n), file=sys.stderr)
+        return False
