3699028ce00b55daff1bf9425de014982d85c5cc
Naol Solomon <naolsolomon10@gmail.com>
2024-01-07 09:44:11 +0300

    fifth commit

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     max = my_list[0]
-    if (my_list == 0):
+    if (len(my_list) == 0):
         return "None"
-    for element in my_list:
-        if (element > max):
-            max = element
+    for element in range(len(my_list)):
+        if (my_list[element] > max):
+            max = my_list[element]
     return max
